3978b7d456f98ad2e27ed04a27839ad1238cd181
devEMEL <dominichenry882@gmail.com>
2020-03-22 17:58:45 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const name = "Ajaebionu Dominic Henry";
+let courses = ['HTML','CSS','JAVASCRIPT'];
+
+console.log(name);
+console.log(courses);
+
+if(courses.length % 2 !== 0){
+    for(let i = 1; i<= 200; i++){
+        if(i % 2 !== 0){
+            console.log(i)
+        }
+    }
+}
